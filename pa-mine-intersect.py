671bafee042ab3d145172f42426a1bf258dbbb18
Theresa Dearden <41048297+tdearden@users.noreply.github.com>
2021-05-01 16:24:10 +0200

    adding .gitignore adding environment.yml adding LICENSE.txt adding requirements.txt adding extent and small code changes for second map

--- pa-mine-intersect.py
+++ pa-mine-intersect.py
@@ -1,133 +1,99 @@
 #%matplotlib notebook
 
-import pandas as pd
-import geopandas as gpd
-import geoplot
-import matplotlib.pyplot as plt
-import cartopy.feature as cfeature
-from cartopy.feature import ShapelyFeature
 import cartopy.crs as ccrs
-import matplotlib.patches as mpatches
-import matplotlib.lines as mlines
-import shapely.geometry as shpgeo
-import contextily as ctx
+import cartopy.feature as cfeature
+import geopandas as gpd
+import matplotlib.pyplot as plt
 
 
 #spatial intersection using spatial index
-def intersect_using_spatial_index(source_gdf, intersecting_gdf):
-    source_sindex = source_gdf.sindex
+def intersect_sindex(source, intersecting):
+    source_sindex = source.sindex
     possible_matches_index = []
 
-    # 'itertuples()' function is a faster version of 'iterrows()'
-    for other in intersecting_gdf.itertuples():
+    for other in intersecting.itertuples():
         bounds = other.geometry.bounds
         c = list(source_sindex.intersection(bounds))
         possible_matches_index += c
 
     # Get unique candidates
-    unique_candidate_matches = list(set(possible_matches_index))
-    possible_matches = source_gdf.iloc[unique_candidate_matches]
+    unique = list(set(possible_matches_index))
+    possible_matches = source.iloc[unique]
 
-    # Conduct the actual intersect
-    result = possible_matches.loc[possible_matches.intersects(intersecting_gdf.unary_union)]
+    # Conduct the intersect
+    result = possible_matches.loc[possible_matches.intersects(intersecting.unary_union)]
     return result
 
 plt.ion()
 
 #Load PA data
 pa = gpd.read_file('data_files/WDPA_DRC_2021_proj.shp')
+pa = pa.to_crs('espg:32733')
 
 #Load mine data and transform to UTM
 mines = gpd.read_file('data_files/osm_rd_congo_infrastructures_mining_fabrication.geojson')
 mines = mines.to_crs('epsg:32733')
-#print(mines.crs)
 
 
 #Extract point features from mining dataset
 mines_points = mines.loc[mines['geomtype']=="points"]
-#print(mines_points.head)
-#mines_points.man_made.unique()
+
 
 #Save selection GeoJSON to SHP and load data
 mines_points.to_file("data_files/osm_congo_mining_points.shp")
 mines_shp = gpd.read_file("data_files/osm_congo_mining_points.shp")
 
-## test plot METHOD 1
-pa_map2, ax = plt.subplots(1, figsize=(10,8))
-ax = pa.plot("DESIG_ENG", legend = True,ax=ax)
-mines_shp.plot(ax=ax, color="red", markersize=1, legend = True)
-pa_map2.suptitle('Protected Areas in the Democratic Republic of Congo and Mines')
-ax.set_axis_off()
-plt.show()
-
-## test plot METHOD 2, no pa data, only some polygons
-
-pa_map = plt.figure(figsize=(10, 10))
-myCRS = ccrs.UTM(33, southern_hemisphere=True)
-ax = plt.axes(projection=ccrs.Mercator())
-
-#Add borders and coastline
-ax.add_feature(cfeature.BORDERS)
-ax.add_feature(cfeature.COASTLINE)
-
-#Extent of map
-xmin, ymin, xmax, ymax = pa.total_bounds
-ax.set_extent([xmin, xmax, ymin, ymax], crs=myCRS)
-
-#Load PA data
-pa_all = ShapelyFeature(pa['geometry'], myCRS,
-                       edgecolor='black',
-                       facecolor='palegreen',
-                       linewidth=1)
-ax.add_feature(pa_all)
-
-mines_fig = ShapelyFeature (mines_shp['geometry'], myCRS,
-                           edgecolor= 'red',
-                           facecolor= 'red',
-                           markersize=1)
-ax.add_feature(mines_fig)
-
-# test plot with
-pa_map2, ax = plt.subplots(1, figsize=(10,8))
-ax = pa.plot("DESIG_ENG", legend = True,ax=ax)
-mines_shp.plot(ax=ax, color="red", markersize=1, legend = True)
-pa_map2.suptitle('Protected Areas in the Democratic Republic of Congo and Mines')
-ax.set_axis_off()
-plt.show()
-
-print(pa_map2)
 
 #intersect using spatial index and count intersections
-pa_mine_intersect = intersect_using_spatial_index(source_gdf=mines_shp, intersecting_gdf=pa)
+pa_mine_intersect = intersect_sindex(source=mines_shp, intersecting=pa)
 intersection_cnt = gpd.sjoin(pa, mines_shp).groupby('NAME').size().reset_index()
 intersection_cnt.head()
+
+#First product: map with all mines and protected areas, intersecting mines in blue
+pa_map, ax = plt.subplots(1, 1, figsize=(10, 10), subplot_kw=dict(projection=ccrs.Mercator()))
+
+xmin, ymin, xmax, ymax = pa.total_bounds
+ax.set_extent([xmin, xmax, ymin, ymax], crs=ccrs.Mercator())
+
+ax.add_feature(cfeature.BORDERS)
+ax.add_feature(cfeature.COASTLINE)
+pa_h = pa.plot("DESIG_ENG", legend=True, ax=ax, cmap="Greens", legend_kwds={'loc': 'lower left'})
+mines_shp.plot(marker='o', ax=ax, color="blue", markersize=2, legend = True)
+pa_mine_intersect.plot(marker='o', ax=ax, color="red", markersize=10)
+
+pa_map.suptitle('Protected Areas and Mining Locations in the Democratic Republic of Congo')
+plt.savefig('drc_pas_mines_intersect.png',dpi=300)
 
 #Merge and plot
 intersection_cnt = intersection_cnt.rename(columns={0: 'intersection_cnt'})
 pa = pa.merge(intersection_cnt, on='NAME')
-pa
+pa.to_csv('pa_mine_intersection_cnt.csv', header=True)
 
 #print maps with mines intersecting over PA
 
 pa_maps = pa.groupby('NAME')
+
 plt.figure()
 
 for i, (name, pa_gdf) in enumerate(pa_maps):
     # create subplot axes in a 3x3 grid
-    ax = plt.subplot(3, 3, i + 1)  # nrows, ncols, axes position
-    # plot the continent on these axes
-    pa_gdf.plot(ax=ax)
-    # pa_mine_intersect.plot(ax=ax, color="blue")
+    ax = plt.subplot(3, 3, i + 1)
+    # plot the pa on these axes
+    pa_gdf.plot(ax=ax, column='NAME', cmap='jet')
+    pa_mine_intersect.plot(ax=ax, color="red", markersize=2, marker='o')
     # set the title
-    ax.set_title(name)
-    # set the aspect
-    # set extent
-    # adjustable datalim ensure that the plots have the same axes size
+    ax.set_title(name, fontsize='7')
+    # set the extent
+    xmin, ymin, xmax, ymax = pa_gdf.total_bounds
+    ax.set_xlim(xmin, xmax)
+    ax.set_ylim(ymin, ymax)
+    # plots have the same axes size
     ax.set_aspect('equal', adjustable='datalim')
     ax.set_axis_off()
 
 plt.tight_layout()
 plt.show()
 
+plt.savefig('drc_pa.png',dpi=300)
 
 
